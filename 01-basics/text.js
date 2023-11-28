154ddf2bd1fc2dc06dc1a00ac585aced2afe7935
Pramitpal2023 <152254861+Pramitpal2023@users.noreply.github.com>
2023-11-28 07:26:24 +0000

    java script basic don

---
+++ 01-basics/text.js
@@ -0,0 +1,9 @@
+console.log("Hello World");
+/*
+1.java script is a High level scripting language used to perform the task on frontend as well as backend
+2.java script use both client site as well as server site
+3.java script is most use programming language long for web devlopment
+4.in java script we use "ESB" version is the leatest one devlopt in 2015
+*Esb=Echma script 6(vanila javascript)
+5.java script is basically used for designing the logic part of our web page
+*/
